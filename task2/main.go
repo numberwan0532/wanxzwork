1e51fe926945d788763f386d7c351d404a9a5a79
wanxz <numberwan0532@163.com>
2025-09-13 09:40:57 +0800

    减少问题提醒

--- task2/main.go
+++ task2/main.go
@@ -2,64 +2,65 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
 	fmt.Println("start...")
-	// num := 10
-	// p := &num
-	// fmt.Println("point value before change:", num)
-	// changePointValue(p)
-	// fmt.Println("point value after change:", num)
+	num := 10
+	p := &num
+	fmt.Println("point value before change:", num)
+	changePointValue(p)
+	fmt.Println("point value after change:", num)
 
-	// s := []int{1, 2, 3, 4, 5}
-	// changeSliceValue(&s)
-	// fmt.Println("slice value after change:", s)
+	s := []int{1, 2, 3, 4, 5}
+	changeSliceValue(&s)
+	fmt.Println("slice value after change:", s)
 
-	// userGo()
+	userGo()
 
-	// time.Sleep(1 * time.Second)
+	time.Sleep(1 * time.Second)
 
-	// tasks := []Task{
-	// 	{Name: "任务1",
-	// 		Func: func() {
-	// 			time.Sleep(2 * time.Second)
-	// 		}},
-	// 	{Name: "任务2", Func: func() {
-	// 		time.Sleep(1 * time.Second)
-	// 	}},
-	// 	{Name: "任务3", Func: func() {
-	// 		time.Sleep(3 * time.Second)
-	// 	}},
-	// }
-	// ScheduleTasks(tasks)
+	tasks := []Task{
+		{Name: "任务1",
+			Func: func() {
+				time.Sleep(2 * time.Second)
+			}},
+		{Name: "任务2", Func: func() {
+			time.Sleep(1 * time.Second)
+		}},
+		{Name: "任务3", Func: func() {
+			time.Sleep(3 * time.Second)
+		}},
+	}
+	ScheduleTasks(tasks)
 
-	// rectangle := Rectangle{}
-	// circle := Circle{}
+	rectangle := Rectangle{}
+	circle := Circle{}
 
-	// rectangle.Area()
-	// rectangle.Perimeter()
+	rectangle.Area()
+	rectangle.Perimeter()
 
-	// circle.Area()
-	// circle.Perimeter()
+	circle.Area()
+	circle.Perimeter()
 
-	// emp := Employee{
-	// 	Person:     Person{Name: "Alice", Age: 30},
-	// 	EmployeeID: "E12345",
-	// }
-	// emp.PrintInfo()
+	emp := Employee{
+		Person:     Person{Name: "Alice", Age: 30},
+		EmployeeID: "E12345",
+	}
+	emp.PrintInfo()
 
-	// ch := make(chan int)
-	// go snedChan(ch)
-	// go readChan(ch)
+	ch := make(chan int)
+	go snedChan(ch)
+	go readChan(ch)
 
-	// time.Sleep(1 * time.Second)
+	time.Sleep(1 * time.Second)
 
-	// ch1 := make(chan int, 5)
-	// go snedChan1(ch1)
-	// go readChan1(ch1)
+	ch1 := make(chan int, 5)
+	go snedChan1(ch1)
+	go readChan1(ch1)
 
-	// time.Sleep(3 * time.Second)
+	time.Sleep(3 * time.Second)
 
 	addTest()
 
