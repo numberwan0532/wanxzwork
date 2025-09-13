dee1152860a34987cbe1277869c53239977b062d
wanxz <numberwan0532@163.com>
2025-09-13 09:39:23 +0800

    减少问题提醒

--- task1/main.go
+++ task1/main.go
@@ -16,11 +16,13 @@ func main() {
 
 	fmt.Println("最长公共前缀:", getLongestPrefix([]string{"flower", "fl", "floight"}))
 
+	fmt.Println("数组加一:", getNumsAdd1([]int{9, 5, 9}))
 	fmt.Println("数组加一:", getNumsAdd2([]int{9, 5, 9}))
 
 	fmt.Println("删除排序数组中的重复项:", deleteRepetition([]int{1, 1, 2, 2, 3, 4, 4, 4, 4, 5}))
 
 	fmt.Println("合并区间:", mergeIntervals([][]int{{1, 3}, {2, 6}, {8, 16}, {15, 18}, {20, 25}}))
 
+	fmt.Println("两数之和:", findTowNumTotal([]int{2, 7, 11, 15, 18, 39}, 25))
 	fmt.Println("两数之和:", findTowNumTotal1([]int{2, 7, 11, 15, 18, 39}, 25))
 }
